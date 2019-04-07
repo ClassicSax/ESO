4ce0d1e354ab359fafd9ef9b6e8d67d6ee5a6b35
Josh Shelton <>
2019-04-07 03:04:56 -0500

    Cleaned up Names Command a little bit, got rid of Airtable helper

--- app/Console/Commands/Names.php
+++ app/Console/Commands/Names.php
@@ -3,8 +3,10 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
-use App\Helpers\Airtable;
 
 class Names extends Command
 {
@@ -39,21 +41,65 @@ class Names extends Command
      */
     public function handle()
     {
-       $names = Airtable::createNamePayload();
-       $records = Airtable::getNames('https://api.airtable.com/v0/appKvtKtRVT9J73tH/ESO?api_key=keyCYYwuTUNjUH1EC');
+       $names = $this->createNamePayload();
+       $records = $this->getNames('https://api.airtable.com/v0/appKvtKtRVT9J73tH/ESO?api_key=keyCYYwuTUNjUH1EC');
        \Log::info($records);
        $this->postNames($names);
 
     }
     private function postNames($names)
     {
-      $payload = Airtable::createNamePayload();
+      $payload = $this->createNamePayload();
       try {
-        $res = Airtable::post('https://api.airtable.com/v0/appKvtKtRVT9J73tH/ESO?api_key=keyCYYwuTUNjUH1EC', $payload);
+        $res = $this->post('https://api.airtable.com/v0/appKvtKtRVT9J73tH/ESO?api_key=keyCYYwuTUNjUH1EC', $payload);
       } catch (\Exception $e) {
         \Log::error('Could not post to airtable', $e);
       }
 
     }
+    public  static function createNamePayload()
+        {
+            return array(
+                'fields' => array(
+                    'Name' => 'Amanda',
+                    'Class' => 'Badass',
+                    'Level' => 'Over 9000',
+                    'Champion Points' => 'Well over 90000',
+                    'Faction' => 'Whatever she wants!',
+                    "Race" => 'YASSSSSSSS QUEEEEEEENNNNNN'
+                )
+            );
+        }
+        public  static function getNames($endpoint)
+            {
+              $body = null;
+
+              try {
+                  $client = new Client(['headers' => ['Content-Type' => 'application/json', 'Authorization' => 'Bearer ' . env('AIRTABLE_APIKEY')]]);
+                  $res = $client->get($endpoint);
+
+                  $body = json_decode($res->getBody());
+              } catch (\Exception $e) {
+                  \Log::error('Could not retrieve names ' . $e);
+              }
+
+              return isset($body->records) ? $body->records : false;
+            }
+            public static function post($endpoint, $payload)
+            {
+                $body = null;
+
+                try {
+                    $client = new Client(['headers' => ['Content-Type' => 'application/json', 'Authorization' => 'Bearer ' . env('AIRTABLE_APIKEY')]]);
+
+                    $res = $client->post($endpoint, array('form_params' => $payload));
+
+                    $body = json_decode($res->getBody());
+                } catch (\Exception $e) {
+                    \Log::error('Could not create name entry' . $e);
+                }
+
+                return $body;
+            }
 
 }
